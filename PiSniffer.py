95cb7f146ab71d4b65c1b4d87e7d8e7551c77fbb
razorback90 <jdray2000@gmail.com>
2023-01-21 21:56:13 -0500

    Trying to open OUI csv file by using r instead of rb

--- PiSniffer.py
+++ PiSniffer.py
@@ -34,7 +34,14 @@ CSVDelim = ','
 FoundAccessPoints = []
 
 OUIMEM = {}
-with open('OUI.txt', 'rb') as OUILookup:
+#with open('OUI.txt', 'rb') as OUILookup:
+#    for line in csv.reader(OUILookup, delimiter='\t'):
+#        if not line or line[0] == "#":
+#            continue
+#        else:
+#            OUIMEM[line[0]] = line[1:]
+
+with open('OUI.txt', 'r') as OUILookup:
     for line in csv.reader(OUILookup, delimiter='\t'):
         if not line or line[0] == "#":
             continue
