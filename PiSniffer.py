48c8df25054fb04ed3c27aad5fca383d10eafbab
razorback90 <jdray2000@gmail.com>
2023-01-21 22:05:36 -0500

    Trying visual studio suggestion

--- PiSniffer.py
+++ PiSniffer.py
@@ -34,19 +34,15 @@ CSVDelim = ','
 FoundAccessPoints = []
 
 OUIMEM = {}
-with open('OUI.txt', 'r') as OUILookup:
+def new_func(OUIMEM, line):
+    OUIMEM[line[0]] = line[1:]
+
+with open('OUI.txt', 'rb') as OUILookup:
     for line in csv.reader(OUILookup, delimiter='\t'):
         if not line or line[0] == "#":
             continue
         else:
-            OUIMEM[line[0]] = line[1:]
-
-#with open('OUI.txt', 'rb') as OUILookup:
-#    for line in csv.reader(OUILookup, delimiter='\t'):
-#        if not line or line[0] == "#":
-#            continue
-#        else:
-#            OUIMEM[line[0]] = line[1:]
+            new_func(OUIMEM, line)
 
 # Initialise GPS to North Pole
 gpsLat = ['NO GPS']
